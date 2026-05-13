de786ad886b6d06df1723e706caa9070b56b5f79
imanelmou <imel1000@stud.ek.dk>
2026-05-13 11:00:25 +0200

    menu

--- src/Menu.java
+++ src/Menu.java
@@ -4,12 +4,14 @@ import java.util.List;
 public class Menu {
 
     List<User> users;
+    List<Parking> zoneList;
     User currentUser;
     TextUI textUI;
     FileIO fileIO;
 
-    public Menu(List<User> users, User currentUser, TextUI textUI, FileIO fileIO) {
+    public Menu(List<User> users,List<Parking> zoner, User currentUser, TextUI textUI, FileIO fileIO) {
         this.users = users;
+        this.zoneList = zoner;
         this.currentUser = currentUser;
         this.textUI = textUI;
         this.fileIO = fileIO;
@@ -64,6 +66,35 @@ public class Menu {
     private void showMenu() {
         System.out.println("\n" + "MENU TESTER - WORKING");
     }
+
+
+    public void showMenuMessage() {
+        boolean parkingRuns = true;
+
+        while (parkingRuns) {
+            textUI.displayMsg("\n--- Tilgængelige muligheder ---");
+            for (Parking p : zoneList ) {
+                textUI.displayMsg(p.getZone() + " (" + p.getSpotTypes() + ")");
+            }
+            textUI.displayMsg("---------------------------\n");
+
+            textUI.displayMsg("1. Parkeringstype");
+            textUI.displayMsg("2. Vælg zone.");
+            textUI.displayMsg("3. Vis vej til valgte parkering .");
+            textUI.displayMsg("4. Log ud af bruger.");
+
+            String choice = textUI.promptString("Vælg en af mulighederne (1-5) ");
+
+            switch (choice) {
+                case "1" -> searchParkingType();
+                case "2" -> searchZone();
+                case "3" -> showMap();
+                case "4" -> parkingRuns = false;
+            }
+
+        }
+    }
+
 
 
 
