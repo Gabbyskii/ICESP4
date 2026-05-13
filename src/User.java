c1345cf526c4bd73a864e930a133793e3f61cbff
imanelmou <imel1000@stud.ek.dk>
2026-05-13 11:24:35 +0200

    menu

--- src/User.java
+++ src/User.java
@@ -4,24 +4,35 @@ public class User {
 
     private int phoneNumber;
     private String mail;
+    private int licensPlate;
     private ArrayList<Car> license = new ArrayList<>();
 
-    public User(int phoneNumber, String mail) {
+    public User(int phoneNumber, String mail, int licensPlate) {
         this.phoneNumber = phoneNumber;
         this.mail = mail;
+        this.licensPlate = licensPlate;
     }
 
-    public void addCarLicense(Car car){
+    public void addCarLicense(Car car) {
         license.add(car);
     }
 
-    public int getPhoneNumber(){
+    public int getPhoneNumber() {
         return phoneNumber;
     }
 
     public String getMail(){
         return mail;
     }
+
+    public int getLicens(){
+        return licensPlate;
+    }
+
+
+
+
+
 
 
     @Override
